fe19c78ca7d395f5e9c2bbc226921447ca44851b
Max Awad <maxawad@apple.com>
2021-02-06 14:56:17 +0400

    Added buy more button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./BuyButton.css";
 
 export default class BuyButton extends Component {
   render() {
     return (
       <button className="button" onClick={this.props.onClick}>
-        {this.props.label ? this.props.label : this.props.buttonName}
+        <Link to={this.props.href ? this.props.href : null}>
+          {this.props.label ? this.props.label : this.props.buttonName}
+        </Link>
       </button>
     );
   }
